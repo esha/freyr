f4fac1b9188f744f1fc8e42c09bebd3b73ec0612
Alex Caroline <alex@esha.com>
2018-08-24 10:06:36 -0700

    Adding a static class that has definitions for the DataSources that GenesisApi currently supports.

---
+++ src/Esha.Genesis.Services.Client/DataSources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esha.Genesis.Services.Client
+{
+    public static class DataSources
+    {
+        public static String AllSources => "All";
+        public static String UserFoods => "UserFoods";
+        public static String EshaFoods => "EshaFoods";
+    }
+}
